8d9c668748afcd2de75e297635e66834afba6d1d
Prince Abalogu <chineduabalogu@yahoo.com>
2020-11-25 20:58:40 +0100

    split methods into modules

--- ruby/public/js/vue.js
+++ ruby/public/js/vue.js
@@ -1,3 +1,7 @@
+import { getConnectToken, getAccessToken, openAuthenticatorEmbed, addAccessToken } from './authentication.js'
+import { fetchResource, fetchChildRecords, fetchJobDetails, fetchJobCandidates, fetchCandidateDetails } from './httpRequests.js'
+import { mouldResourceTableItems, onRowSelected, onJobSelected, onCandidateSelected, swapTab } from './utils.js'
+
 const vueApp = new Vue({
   el: '#app',
   data: {
@@ -5,12 +9,14 @@ const vueApp = new Vue({
     addingToken: false,
     submittingConnect: false,
     requestingResource: false,
+    selectedItem: null,
+    selectedJobId: null,
+    fetchingDetails: false,
+    fetchedJobDetails: null,
+    fetchedCandidateDetails: null,
     employeesModal: true,
     jobsModal: false,
     payrollsModal: false,
-    selectedItem: null,
-    fetchingJobDetails: false,
-    jobs: [],
     resourceData: null,
     employeesFields: [
       {
@@ -65,6 +71,25 @@ const vueApp = new Vue({
       },
     ],
     payrolls: [],
+    candidatesFields: [
+      {
+        key: 'full_name',
+        sortable: false,
+      },
+      {
+        key: 'email',
+        sortable: false,
+      },
+      {
+        key: 'phone_number',
+        sortable: false,
+      },
+      {
+        key: 'address',
+        sortable: false,
+      }
+    ],
+    candidates: [],
   },
   computed: {
     credentialsAdded() {
@@ -83,196 +108,20 @@ const vueApp = new Vue({
     }
   },
   methods: {
-    fetchRequest(record) {
-      const vm = this
-
-      vm.requestingResource = true
-
-      fetch(`/${record}/fetch_resource/${vm.access_token}`)
-        .then(response => response.json())
-        .then((data) => {
-          vm.requestingResource = false
-          vm.resourceData = data
-          vm.resourceData.status = 'success'
-          vm.mouldResourceTableItems(data, record)
-        })
-        .catch((error) => {
-          vm.requestingResource = true
-          console.error('Error msg:', error);
-        })
-    },
-    getConnectToken(companyName, authenticatorEnv) {
-      const vm = this
-
-      vm.submittingConnect = true
-
-      fetch('/connect_tokens/create')
-        .then(response => response.json())
-        .then((data) => {
-          vm.submittingConnect = false
-          vm.openAuthenticatorEmbed(data.data.id, companyName, authenticatorEnv) 
-        })
-        .catch((error) => {
-          vm.submittingConnect = false
-          console.error('Error msg:', error)
-        })
-    },
-    getAccessToken(publicToken) {
-      const vm = this
-
-      fetch(`/public_token/exchange/${publicToken}`)
-        .then(response => response.json())
-        .then((data) => {
-          vm.access_token = data.data.id
-        })
-        .catch((error) => {
-          console.error('Error msg:', error)
-        })
-    },
-    openAuthenticatorEmbed(connectToken, companyName, authenticatorEnv) {
-      const vm = this
-
-      vm.submittingConnect = false
-
-      NCConnector.create({
-        company_name: companyName,
-        connect_token: connectToken,
-        authenticator_env: authenticatorEnv,
-        successCallback: (token) => {
-          if (authenticatorEnv === 'sandbox') {
-            vm.access_token = token.id
-
-            return
-          }
-          vm.getAccessToken(token.id)
-        },
-        failureCallback: (error_message) => { console.error(error_message) }
-      })
-    },
-    addAccessToken() {
-      const vm = this
-
-      vm.addingToken = true
-    },
-    mouldResourceTableItems(data, record){
-      const vm = this
-
-      const itemsArray = []
-
-      switch (record) {
-        case 'employees': {
-          data.data.forEach((item) => {
-            const itemToSave = {
-              id: item.id,
-              full_name: item.attributes.first_name + item.attributes.last_name,
-              title: item.attributes.title,
-              work_email: item.attributes.work_email,
-              salary: item.attributes.salary,
-              record: record,
-            }
-            itemsArray.push(itemToSave)
-          })
-
-          vm[record] = itemsArray
-
-          return
-        }
-        case 'jobs': {
-          data.data.forEach((item) => {
-            const itemToSave = {
-              id: item.id,
-              name: item.attributes.name,
-              location: item.attributes.location,
-              remote: item.attributes.remote,
-              status: item.attributes.status,
-              record: record,
-            }
-            itemsArray.push(itemToSave)
-          })
-
-          vm[record] = itemsArray
-
-          return
-        }
-        case 'payrolls': {
-          data.data.forEach((item) => {
-            const itemToSave = {
-              id: item.id,
-              check_data: item.attributes.check_data,
-              pay_period_end_date: item.attributes.pay_period_end_date,
-              pay_period_start_date: item.attributes.pay_period_start_date,
-              record: record,
-            }
-            itemsArray.push(itemToSave)
-          })
-
-          vm[record] = itemsArray
-
-          return
-        }
-      }
-    },
-    onRowSelected(items) {
-      const vm = this
-
-      vm.selectedItem = items[0]
-
-      vm.$refs['my-modal'].show()
-    },
-    fetchChildRecords(item) {
-      const vm = this
-
-      switch (item.record) {
-        case 'jobs': {
-
-          return
-        }
-      }
-    },
-    swapTab(record) {
-      const vm = this
-
-      const recordModal = record + 'Modal'
-      vm[recordModal] = true
-
-      vm.resourceData = null
-
-      switch (record) {
-        case 'jobs': {
-          vm.employeesModal = false
-          vm.payrollsModal = false
-
-          return
-        }
-        case 'employees': {
-          vm.jobsModal = false
-          vm.payrollsModal = false
-
-          return
-        }
-        case 'payrolls': {
-          vm.jobsModal = false
-          vm.employeesModal = false
-
-          return
-        }
-      }
-    },
-    fetchJobDetails() {
-      const vm = this
-
-      vm.fetchingJobDetails = true
-
-      fetch(`/show_job/${vm.selectedItem.id}`)
-        .then(response => response.json())
-        .then((data) => {
-          vm.fetchingJobDetails = false
-          console.log(data)
-        })
-        .catch((error) => {
-          console.error('Error msg:', error)
-        })
-    },
+    fetchResource,
+    getConnectToken,
+    getAccessToken,
+    openAuthenticatorEmbed,
+    addAccessToken,
+    mouldResourceTableItems,
+    onJobSelected,
+    onRowSelected,
+    onCandidateSelected,
+    fetchChildRecords,
+    swapTab,
+    fetchJobDetails,
+    fetchJobCandidates,
+    fetchCandidateDetails,
   },
 })
 
